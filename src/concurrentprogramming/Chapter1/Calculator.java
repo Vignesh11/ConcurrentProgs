588035c255a8bb63e5243f34189d215db8c694d5
Vignesh <vickzzkct@gmail.com>
2015-06-20 14:33:50 +0530

    Calc updated prog

--- src/concurrentprogramming/Chapter1/Calculator.java
+++ src/concurrentprogramming/Chapter1/Calculator.java
@@ -1,18 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package concurrentprogramming.Chapter1;
 
+
+package concurrentprogramming.Chapter1;
 /**
  *
  * @author Vignesh
  */
-public class Calculator {
-    
+public class Calculator { 
     public static void main(String[] args) {
-        for(int i =0 ;i<10;i++){
+        for(int i =1 ;i<10;i++){
             Processor process = new Processor(i);
             Thread t1 = new Thread(process);
             t1.start();     
@@ -27,10 +22,8 @@ class Processor implements Runnable {
     }
     @Override
     public void run() {
-       for(int i = 0;i<10;i++){
+       for(int i = 1;i<10;i++){
            System.out.printf(" %s : %d * %d = %d  \n", Thread.currentThread().getName(),i,number,i*number);
        }
-    }
-    
-    
+    }    
 }
